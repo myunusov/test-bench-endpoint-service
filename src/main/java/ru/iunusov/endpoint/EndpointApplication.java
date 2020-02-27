8bebe4731ed0e76d0e32815b2452a81e7a1807f4
Maxim Iunusov <maxim@iunusov.ru>
2020-02-27 23:09:20 +0300

    fix

--- src/main/java/ru/iunusov/endpoint/EndpointApplication.java
+++ src/main/java/ru/iunusov/endpoint/EndpointApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class EndpointApplication {
   public static void main(String[] args) {
-    SpringApplication.run(GatewayApplication.class, args);
+    SpringApplication.run(EndpointApplication.class, args);
   }
 }
